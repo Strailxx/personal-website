d34eb47792ae125626ad123b6088dd21421b2880
Jarod Primeau <primeaujarod@gmail.com>
2025-06-12 22:07:03 -0300

    Kinda fixed bubbles?
    Bubbles now are able to make it up and down by splitting them into different "wrappers" Not sure how permanent this is or if its going to work for the overall idea, Currently splitting them evenly and might make animating them quite hard

--- src/components/lavalamp/lavalamp.jsx
+++ src/components/lavalamp/lavalamp.jsx
@@ -8,17 +8,23 @@ const lavalamp = () => {
       <div class="frame">
         <div class="container">
           <div class="gooey">
-            <div class="bubble"></div>
-            <div class="bubble"></div>
-            <div class="bubble"></div>
-            <div class="bubble"></div>
-            <div class="bubble"></div>
-            <div class="bubble"></div>
-            <div class="bubble"></div>
-            <div class="bubble"></div>
-            <div class="bubble"></div>
-            <div class="bubble"></div>
-            <div class="bubble"></div>
+            <div class="bubbleTop"></div>
+            <div class="bubbleBot"></div>
+            <div class="bubbleWrapper">
+              <div class="bubble"></div>
+              <div class="bubble"></div>
+              <div class="bubble"></div>
+            </div>
+            <div class="bubbleWrapper">
+              <div class="bubble"></div>
+              <div class="bubble"></div>
+              <div class="bubble"></div>
+            </div>
+            <div class="bubbleWrapper">
+              <div class="bubble"></div>
+              <div class="bubble"></div>
+              <div class="bubble"></div>
+            </div>
           </div>
         </div>
       </div>
